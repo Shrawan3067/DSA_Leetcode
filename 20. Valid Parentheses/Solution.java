123214e36eabe424ebac150884ca940ab4a097b4
Shrawan3067 <shrawansah3067@gmail.com>
2025-07-19 23:09:09 +0530

    Day 8

---
+++ 20. Valid Parentheses/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stk.push(ch);
+            } else {
+                if (stk.isEmpty()) {
+                    return false;
+                } else {
+                    if ( (stk.peek() == '(' && ch == ')') || (stk.peek() == '{' && ch == '}') 
+                    || (stk.peek() == '[' && ch == ']') ) {
+                        stk.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if (stk.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
